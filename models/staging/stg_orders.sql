95e66466cf830104fccb2567abf9166967f62fcf
him_y2u@yahoo.com <him_y2u@yahoo.com>
2023-04-09 21:20:58 +0000

    developed staging orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,17 @@
+select 
+-- from raw_orders
+o.orderid,o.orderdate,o.shipdate,o.shipmode
+
+, ordersellingprice-ordercostprice as orderprofit
+, ordersellingprice,ordercostprice
+--from raw_customer
+,c.customername,c.segment, c.country
+-- from raw product
+, p.category, p.productname, p.subcategory
+
+ from {{ ref('raw_orders') }} as o
+ left join {{ ref('raw_customer') }} as c
+ on o.customerid = c.customerid 
+ left join {{ ref('raw_product') }} p
+ on o.productid = p.productid
+ 
