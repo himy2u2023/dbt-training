60bebadf79fa7640fb289c6837e527eaedf3e5fa
him_y2u@yahoo.com <him_y2u@yahoo.com>
2023-04-13 05:32:52 +0000

    Added seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -10,8 +10,11 @@ o.orderid,o.orderdate,o.shipdate,o.shipmode
 -- from raw product
 ,p.productid, p.category, p.productname, p.subcategory
 ,{{ markup('ordersellingprice','ordercostprice') }} as markup
+,d.delivery_team
  from {{ ref('raw_orders') }} as o
  left join {{ ref('raw_customer') }} as c
  on o.customerid = c.customerid 
  left join {{ ref('raw_product') }} p
  on o.productid = p.productid
+ left join {{ ref('delivery_team') }} as d
+ on o.shipmode = d.shipmode
